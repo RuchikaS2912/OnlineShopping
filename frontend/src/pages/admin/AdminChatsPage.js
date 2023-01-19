ddbdc973aa4384fd0aeb8335b41534fbfe2c9cc5
Ruchika <Ruchika.Sreedhar1@ibm.com>
2023-01-19 22:53:28 +0530

    Changes to frontend

--- frontend/src/pages/admin/AdminChatsPage.js
+++ frontend/src/pages/admin/AdminChatsPage.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React from 'react';
+import {Row,Col} from 'react-bootstrap';
+import AdminSideBar from '../../components/admin/AdminSideBar';
+import AdminChatRoomComponent from '../../components/admin/AdminChatRoomComponent';
 
-const AdminChatspage = () => {
-  return <div>AdminChatspage</div>;
-};
+const AdminChatspage =()=>{
+  return(
+    <Row>
+    <Col md={2}><AdminSideBar/></Col>
+    <Col md={10}><Row><AdminChatRoomComponent/></Row></Col>
+    </Row>
+  )
+}
 
 export default AdminChatspage;
--- frontend/src/pages/admin/AdminUsersPage.js
+++ frontend/src/pages/admin/AdminUsersPage.js
@@ -1,7 +1,74 @@
 import React from "react";
+import { Col, Table, Row,Button } from "react-bootstrap";
+import { ArchiveFill, Check, PencilSquare, X } from "react-bootstrap-icons";
+import { Link } from "react-router-dom";
+import AdminSideBar from "../../components/admin/AdminSideBar";
+import { LinkContainer } from "react-router-bootstrap";
 
 const AdminUsersPage = () => {
-  return <div>AdminUsersPage</div>;
+
+  const deleteHandler = () => {
+    if (window.confirm("Are you sure ?")) alert("User deleted");
+  };
+
+
+  return (
+    <Row className="m-5">
+      <Col md={2}>
+        <AdminSideBar />
+      </Col>
+      <Col md={10}>
+      <h1>User List</h1>
+        <Table striped bordered hover responsive>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Is Admin</th>
+              <th>Edit/Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Array.from({ length: 10 }).map((item, idx) => (
+              <tr>
+                <td>{idx + 1}</td>
+                <td>Mark</td>
+                <td>Twain</td>
+                <td>email@email.com</td>
+                {idx % 2 === 0 ? (
+                  <td>
+                    <Check color="green" />
+                  </td>
+                ) : (
+                  <td>
+                    <X color="red" />
+                  </td>
+                )}
+                <td>
+                  <LinkContainer to="/admin/edit-users">
+                    <Button className="btn-sm">
+                      <PencilSquare />
+                    </Button>
+                  </LinkContainer>
+                  {" / "}
+                  <Button
+                    className="btn-sm"
+                    variant="danger"
+                    onClick={deleteHandler}
+                  >
+                    {" "}
+                    <ArchiveFill />
+                  </Button>
+                </td>
+                </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Col>
+    </Row>
+  );
 };
 
 export default AdminUsersPage;
